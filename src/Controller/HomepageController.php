44c6dc3f556ad7644a7a8536c277c69cac4fbfdc
DesirThomas <thomasdesir.pro@gmail.com>
2020-07-14 23:27:03 +0200

    CSS/SCSS/Route/Trans/Contact

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,16 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomepageController extends AbstractController
 {
+
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+
     /**
      * @Route("/", name="homepage")
      */
     public function index()
     {
         return $this->render('homepage/index.html.twig', [
-            'pagetitle' => 'Accueil',
+            'pagetitle' => ucfirst($this->translator->trans('homepage.title')),
         ]);
+    }
+
+
+    /**
+     * @Route({"fr": "/contact", "en": "/contact"}, name="contact")
+     */
+    public function contact()
+    {
+        return $this->render( 'contact/index.html.twig',
+            [
+                'pagetitle' => ucfirst($this->translator->trans('contact.title')),
+            ] );
     }
 }
